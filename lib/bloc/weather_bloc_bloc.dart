de2e0615f1a788f1740bcf6903fc4427307f5f6f
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-11-22 15:36:31 +0000

    homeScreen Ui done

--- lib/bloc/weather_bloc_bloc.dart
+++ lib/bloc/weather_bloc_bloc.dart
@@ -1,13 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:weather/weather.dart';
 
 part 'weather_bloc_event.dart';
 part 'weather_bloc_state.dart';
 
 class WeatherBlocBloc extends Bloc<WeatherBlocEvent, WeatherBlocState> {
   WeatherBlocBloc() : super(WeatherBlocInitial()) {
-    on<WeatherBlocEvent>((event, emit) {
-      // TODO: implement event handler
+    on<FetchWeather>((event, emit) {
+      emit(WeatherBlocLoading());
+      try {
+        WeatherFactory wf =
+            WeatherFactory("apikey", language: Language.ENGLISH);
+        emit(WeatherBlocSuccess());
+      } catch (e) {
+        emit(WeatherBlocFailure());
+      }
     });
   }
 }
